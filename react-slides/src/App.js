b4b09a788d1d627193282cd0ae79d486ed3bf1b2
GerardoRoman <offthegrd3657@gmail.com>
2023-03-20 08:51:07 -0400

    release date wasn't showing up, rechecked and it was a misspelling of the word 'release'

--- react-slides/src/App.js
+++ react-slides/src/App.js
@@ -4,7 +4,7 @@ import filmData from './film-data.json';
 
 function App() {
   return (
-    <div className='section'>
+    <div>
       <div>
         <div>
           {
@@ -33,18 +33,17 @@ function Gallery() {
 
 let data = filmData[index];
 return (
-    <>
+    <div className='section' style={{border: '2px solid black', margin: '10px', padding: '4px'}}>
       <h2 className='title'>{data.title}</h2>
       <h3 className='og-title'>{data.original_title}</h3>
-      <h4 className='release-date'>{data.realease_date}</h4>
+      <h4 className='release-date'>{data.release_date}</h4>
       <h3 className='description'>{data.description}</h3>
       <div className='image'><img src={data.image} alt='cover' /></div>
 
-      <button onClick={handleBackClick} disabled={index === 0}>Back</button>
-      <button onClick={handleRestartClick} disabled={index === 0}>Restart</button>
-      <button onClick={handleNextClick} disabled={index === filmData.length - 1}>Next</button>
-      
-    </>
+      <button className='back-button' onClick={handleBackClick} disabled={index === 0}>Back</button>
+      <button className='restart-button' onClick={handleRestartClick} disabled={index === 0}>Restart</button>
+      <button className='next-button' onClick={handleNextClick} disabled={index === filmData.length - 1}>Next</button>
+    </div>
   )
 }
 
